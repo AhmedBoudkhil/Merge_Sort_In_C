90c94330cbc07f764212e7f5c524e190203e5cab
Ahmed  BOUDKHIL <90070683+ahmedhmida962@users.noreply.github.com>
2024-08-08 20:57:47 +0200

    Update mergeSort.c

--- mergeSort.c
+++ mergeSort.c
@@ -1,53 +1,64 @@
 #include <stdio.h>
+#include <stdlib.h>
 
+void merge(int arr[], int left[], int right[], int leftSize, int rightSize) {
+    int i = 0, j = 0, k = 0;
 
-
-
-void mergeSort(int arr[], int length){
-    if(length <= 1)  return;
-
-    int midd = length /2;
-    int leftArr[midd];
-    int rightArr[length - midd];
-
-    for(int i = 0; i<length; i++){
-        if(i < midd) leftArr[i] = arr[i];
-        else  rightArr[i] = arr[i];   
+    // Merge the left and right arrays
+    while (i < leftSize && j < rightSize) {
+        if (left[i] <= right[j]) {
+            arr[k++] = left[i++];
+        } else {
+            arr[k++] = right[j++];
+        }
     }
-    mergeSort(leftArr,midd);
-    mergeSort(rightArr,length - midd);
-    merge(arr,leftArr,rightArr,midd,length - midd);
 
+    // Copy any remaining elements of left[]
+    while (i < leftSize) {
+        arr[k++] = left[i++];
+    }
+
+    // Copy any remaining elements of right[]
+    while (j < rightSize) {
+        arr[k++] = right[j++];
+    }
 }
 
-void merge(int arr[], int leftArr[],int rightArr[],int leftSize, int rightSize){
-    int r = 0;
-    int l = 0;
-    int tracker = 0;
-    while( l < leftSize && r < leftSize ){
-        if(leftArr[l] <= rightArr[r]){
-            arr[tracker] = leftArr[l];
-            l++;
-        }else{
-            arr[tracker] = rightArr[r];
-            r++;
-        }
-        tracker++;
-    }
+void mergeSort(int arr[], int n) {
+    if (n < 2) return;  // Base case: if the array size is less than 2, it's already sorted
 
-    while( l < leftSize){
-        arr[tracker] = leftArr[l];
-        l++;
-    }
-    while( r< rightSize){
-        arr[tracker] = rightArr[l];
-        r++;
-    }
+    int middle = n / 2;
+    
+    // Dynamically allocate memory for the left and right subarrays
+    int *left = (int*)malloc(middle * sizeof(int));
+    int *right = (int*)malloc((n - middle) * sizeof(int));
+
+    // Copy data to the left and right subarrays
+    for (int i = 0; i < middle; i++) left[i] = arr[i];
+    for (int i = middle; i < n; i++) right[i - middle] = arr[i];
+
+    // Recursive calls to sort the subarrays
+    mergeSort(left, middle);
+    mergeSort(right, n - middle);
+
+    // Merge the sorted subarrays
+    merge(arr, left, right, middle, n - middle);
+
+    // Free the dynamically allocated memory
+    free(left);
+    free(right);
 }
 
 int main() {
-    int arr[] = {3,1,9,232,242};
- 
+    int arr[] = {10, 3, 23, 7, 9, 6};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    mergeSort(arr, n);
+
+    // Print the sorted array
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
 
     return 0;
 }
